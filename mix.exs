98aadcb05897139ee8b3712358612c1edecf44f3
Aestimo K <thrivepro.agency@gmail.com>
2023-08-04 13:51:50 +0300

    Refreshed repo

--- mix.exs
+++ mix.exs
@@ -21,8 +21,8 @@ defmodule SampleMath.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
-      {:doctor, "~> 0.10.0", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:doctor, "~> 0.21.0", only: :dev}
     ]
   end
 end
